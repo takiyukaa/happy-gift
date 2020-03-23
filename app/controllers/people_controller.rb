91fa1fc64f471715145850a608393061085a61a5
Yuka Takiyama <yukatakiyama27@gmail.com>
2020-03-23 16:21:43 +0900

    link people index, items index to home, added edit page for person

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -7,4 +7,23 @@ class PeopleController < ApplicationController
   def show
     @person = Person.find(params[:id])
   end
+
+  def edit
+    @person = Person.find(params[:id])
+  end
+
+  def update
+    @person = Person.find(params[:id])
+    if @person.update(person_params)
+      redirect_to person_path(@person)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:name, :birthday, :group, :address, :description)
+  end
 end
